39ac685d63863334da926e2abdb8cb1b63810437
Nikolas Ferenčák <experimentproduction@gmail.com>
2021-11-16 18:48:13 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import './App.css';
 import { useState, useCallback, useEffect } from "react";
 import axios from 'axios';
 
-function App() {
+import './App.css';
+
+const App = () => {
   const [text, setText] = useState<string>("");
   const [parsedData, setParsedData] = useState<string>(""); 
   const [title, setTitle] = useState<string>("");
-  const [result, setResult] = useState<any[]>([]);
+  const [result, setResult] = useState<any[]>([null]);
 
   const findIt = useCallback(
     async () => {
       setParsedData("");
       setResult([]);
-      const { data } = await axios.request({
-        method: 'GET',
-        url: `https://cs.wikipedia.org/w/api.php?origin=%2A&format=json&action=query&prop=extracts&redirects=1&explaintext=&titles=${text}`,
-      });
+      const { data } = await axios.get(`https://cs.wikipedia.org/w/api.php?origin=%2A&format=json&action=query&prop=extracts&redirects=1&explaintext=&titles=${text}`);
       if(!data.query) return;
-      if(data.query?.length === 0) return;
-      const firstKey = Object.keys(data.query.pages)[0];
-      const { title, extract } = data.query.pages[firstKey];
+      const { title, extract } = data.query.pages[Object.keys(data.query.pages)[0]];
+      if(!extract) return;
       setTitle(title);
       setParsedData(extract.replace(/\([^()]*\)/g, ''));
     }, [text]
@@ -27,23 +24,24 @@ function App() {
 
   useEffect(
     () => {
-      if(parsedData.length === 0) return;
-      for(let i=0;i<3;i++) {
-        setResult(prevResult => [...prevResult, parsedData.split('. ', i+1)[i] + '. ']);
-      }
+      if(!parsedData) return;
+      let parsedSentence = parsedData.split('==', 1)[0];
+      setResult(prevResult => [...prevResult, parsedSentence]);
     }, [parsedData]
-  )
+  );
   
   return (
     <div className="App">
       <p>Yo, dude najdi něco: </p>
       <input type="text" onKeyUp={ (e: any) => setText(e.target.value) } />
       <button onClick={ findIt }>LES GO</button>
-      { setParsedData.length &&
+      { result.length > 0 ?
         <div className="text-output">
           <p>{ title }</p>
           { result }
         </div>
+        : 
+        <p>Nic jsem nenašel...</p>
       }
     </div>
   );
